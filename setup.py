1e520f0342a234a04c17a55abbf116ef5ce7a86e
Alireza Meskin <alireza.meskin@gmail.com>
2014-04-03 14:11:53 +0430

    Fixed bug in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyrgraph',
-    version='0.1',
+    version='0.1.2',
     description='Pyrgraph is a Redis-backed graph database',
     url='http://github.com/alireza-m/pyrgraph',
     author='Alireza Meskin',
@@ -10,7 +10,6 @@ setup(
     license='MIT',
     packages=['pyrgraph'],
     zip_safe=False,
-    long_description=open('README.md').read(),
     install_requires=[
         "redis > 2.9.0"
     ]
